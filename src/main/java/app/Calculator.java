855605589d71873dd8301ccdf6df60b082204b98
Ira <ira350520@gmail.com>
2024-11-09 12:59:55 +0300

    solution homework

---
+++ src/main/java/app/Calculator.java
@@ -0,0 +1,7 @@
+package app;
+
+public class Calculator {
+    public double sum(double a, double b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/app/CalculatorTest.java
@@ -0,0 +1,29 @@
+package app;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        calculator = null;
+    }
+
+    @Test
+    void sum() {
+        int a = 5;
+        int b = 10;
+        assertEquals(a +b, calculator.sum(a, b));
+    }
+}
